c87c17efa08258a46ef0845903d853ddc603ff09
sgreco <sgreco2002@libero.it>
2017-01-09 05:42:47 -0800

    Add THA anmd Monitoring servers

--- THA/THA_VLAN_DATA_CREATION.tf
+++ THA/THA_VLAN_DATA_CREATION.tf
@@ -2,8 +2,8 @@
 resource "clc_server" "RCREDATAREP" {                                //variabile nome macchina es FE -> sul cloud diventa FE01 / 02 etc
   //count = "${var.VGLOBALVM.["VM.MinInstances"]}"
   name_template = "${var.VHOSTNAME.["THA.REP"]}"
-  source_server_id = "XX"
-  group_id = "${clc_group.GROUP1THAAPP.id}"                        //variabile nome folder dove posizionare la macchina
+  source_server_id = "VA2OTA2CREP0101"
+  group_id = "${clc_group.GROUP1THADATA.id}"                        //variabile nome folder dove posizionare la macchina
   cpu = "${var.VGLOBRESOURCE.["DBS.CPU"]}"                        //variabile CPU CACHE VM
   memory_mb = "${var.VGLOBRESOURCE.["DBS.RAM"]}"                  //variabile RAM CACHE VM
   password = "${var.VGLOBALVM.["RH-WIN.Admin.Passwd"]}"             //variabile password AVS VM
